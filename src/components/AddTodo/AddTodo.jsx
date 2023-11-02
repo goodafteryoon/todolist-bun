a021c50d57d5f1b92d4cbbdb95e04c59771fad71
yunna <homer_j@hanmail.net>
2023-11-02 17:23:25 +0900

    style : 전체 style 적용 (post css)

--- src/components/AddTodo/AddTodo.jsx
+++ src/components/AddTodo/AddTodo.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
+import styles from './AddTodo.module.css';
 
 export default function AddTodo({ onAdd }) {
   const [text, setText] = useState('');
@@ -17,14 +18,15 @@ export default function AddTodo({ onAdd }) {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form className={styles.form} onSubmit={handleSubmit}>
       <input
         type='text'
         placeholder='Add Todo'
         value={text}
         onChange={handleChange}
+        className={styles.input}
       />
-      <button>Add</button>
+      <button className={styles.button}>Add</button>
     </form>
   );
 }
