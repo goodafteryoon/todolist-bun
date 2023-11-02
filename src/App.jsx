1578aa95c731f04f67c93ed37d4af3144f801677
yunna <homer_j@hanmail.net>
2023-11-02 15:59:50 +0900

    feat : 리스트 상태에 따른 filter 기능 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+import Header from './components/Header/Header';
 import TodoList from './components/TodoList/TodoList';
+import { useState } from 'react';
 
+const filters = ['all', 'active', 'completed'];
 function App() {
+  const [filter, setFilter] = useState(filters[0]);
+
   return (
     <>
-      <TodoList />
+      <Header filters={filters} filter={filter} onFilterChange={setFilter} />
+      <TodoList filter={filter} />
     </>
   );
 }
